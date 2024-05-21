a61cfe34f20d9496b56bf934ef52953d030ac6c7
ВАШÐAnastasia <anastasija2003@mail.ru>
2024-05-21 21:10:25 +0300

    HW15

---
+++ app/src/main/java/com/example/hw15/database/WordsDatabase.kt
@@ -0,0 +1,12 @@
+package com.example.hw15.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Word::class],
+    version = 1
+)
+abstract class WordsDatabase : RoomDatabase() {
+    abstract fun wordDao(): WordsDao
+}
